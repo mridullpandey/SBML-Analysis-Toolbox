4fd04485ca1388732a8276ce6a721f1b2d6fe0fe
Kevin Rodden <kevin.rodden@outlook.com>
2018-03-21 10:07:34 -0400

    update

---
+++ delete_me.py
@@ -0,0 +1,7 @@
+nameIndexDictionary = {'Model':1,'Compartments':2}
+
+print nameIndexDictionary['Model']
+
+sample_Name = "Model"
+
+[value for key, value in programs.items() if 'new york' in key.lower()]
--- main.py
+++ main.py
@@ -1,31 +1,27 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-#  main.py
-#  
-#  Copyright 2018 kevin <kevin@galliumos>
-#  
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#  
-#  
+from PyQt4 import QtGui
+import sys
+import design
 
-
-def main(args):
-    return 0
-
+class ExampleApp(QtGui.QMainWindow, design.Ui_MainWindow):
+	def __init__(self, parent=None):
+		super(ExampleApp,self).__init__(parent)
+		self.setupUi(self)
+		
+		self.treeWidget.currentItemChanged.connect(self.updatePageWidget)
+		
+	def updatePageWidget(self):
+		nameIndexDictionary = {'Model':1,'Compartments':2}
+		
+		
+		self.treeWidget.currentItem().text(0)
+			
+	
+		
+def main():
+	app = QtGui.QApplication(sys.argv)
+	form = ExampleApp()
+	form.show()
+	app.exec_()
+	
 if __name__ == '__main__':
-    import sys
-    sys.exit(main(sys.argv))
+	main()
